a80d6d08918f889679676f4b45e709a8ed7dc826
Hridoy Khandakar <knhridoy150@gmail.com>
2022-06-09 12:45:54 +0600

    improved

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-let num1 = Math.floor(Math.random() * 6) + 1;
-let num2 = Math.floor(Math.random() * 6) + 1;
+const btn = document.querySelector(".btn");
 
-document.querySelector(".p1").src = `images/dice${num1}.png`;
-document.querySelector(".p2").src = `images/dice${num2}.png`;
+btn.addEventListener("click", play);
 
-if (num1 === num2) {
-  document.querySelector(".title").innerText = "Draw";
-} else if (num1 < num2) {
-  document.querySelector(".title").innerText = "player 2 win";
-} else {
-  document.querySelector(".title").innerText = "player 1 win";
+function play(e) {
+  e.preventDefault();
+
+  let num1 = Math.floor(Math.random() * 6) + 1;
+  let num2 = Math.floor(Math.random() * 6) + 1;
+  document.querySelector(".img1").src = `images/dice${num1}.png`;
+  document.querySelector(".img2").src = `images/dice${num2}.png`;
+
+  if (num1 > num2) {
+    document.querySelector(".title").innerText = "player 1 win";
+  } else if (num1 < num2) {
+    document.querySelector(".title").innerText = "player 2 win";
+  } else {
+    document.querySelector(".title").innerText = "Draw";
+  }
+  document.querySelector(".btn").innerText = "Play Again";
 }
-const myfunc = () => {};
